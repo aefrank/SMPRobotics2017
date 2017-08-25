5316a1a969afec1bd7ea9d6eed6ecc8a5c2b1595
aefrank <anf@seas.upenn.edu>
2017-08-25 14:29:32 -0400

    Added comments
    Method description comments added to add clarity to library. Updated TobiPro::setSpeed() method to update TobiPro::_motorSpeed[motor]

--- TobiPro/TobiPro.cpp
+++ TobiPro/TobiPro.cpp
@@ -2,7 +2,7 @@
 
 #include <TobiPro.h>
 
-
+//************************** LIBRARY METHODS ***************************//
 /*          SERIALSETUP()
     Set up Serial communication. Begins Serial communication at 9600 baud, sets timeout at
     10 ms, waits 5 seconds for Serial comm to begin before returning and allowing program
@@ -22,6 +22,10 @@ void serialSetup(){
 
 
 //************************** CLASS METHODS ***************************//
+/*      CONSTRUCTOR
+    Creates a new TobiPro object. TobiPro is an extension of the Tobi class from Tobi.h.
+    Sets motor indices to 0 to NUM_MOTORS as default; motor speeds to 0, and filters to on.
+*/
 TobiPro::TobiPro(){
   for(int i = 0; i < NUM_MOTORS; i++){
     TobiPro::_motorIndices[i] = i;
@@ -33,28 +37,65 @@ TobiPro::TobiPro(){
   TobiPro::filterInputs(true);
 }
 
-
+/*      SETMOTORINDICES()
+    If active motors are not numbered simply 0 through NUM_MOTORS, this method can be used to
+    set TobiPro::_motorIndices to the correct indices for the active motors.
+    INPUTS:   - int* motInd (pointer to an integer array of size NUM_MOTORS corresponding to
+                    the motors that are active for this TobiPro).
+    OUTPUTS:  - None.
+    UPDATES:  - TobiPro::_motorIndices
+    EFFECTS:  - None.
+*/
 void TobiPro::setMotorIndices(int* motInd){
     for(int i = 0; i < NUM_MOTORS; i++){
       TobiPro::_motorIndices[i] = motInd[i];
     }
 }
 
+/*      FILTERINPUTS()
+    Sets TobiPro to filter or not filter encoder values with a two pole low pass filter, defined
+    in TobiFilterManager.
+    INPUTS:   - bool onOff (true for filtering on, false for filtering off).
+    OUTPUTS:  - None.
+    UPDATES:  - TobiPro::filter.
+    EFFECTS:  - None.
+*/
 void TobiPro::filterInputs(bool onOff){
     TobiPro::filter.onOff(onOff);
 }
 
+
+/*      UPDATE()
+    Calculates speed and updates filters for each motor.
+    INPUTS:   - None.
+    OUTPUTS:  - None.
+    UPDATES:  - TobiPro::_motorSpeed.
+    EFFECTS:  - None.
+*/
 void TobiPro::update(){
   for (int i = 0; i < NUM_MOTORS; i++){
       TobiPro::_motorSpeed[TobiPro::_motorIndices[i]] = TobiPro::calcSpeed(i); // calc speed; also updates filters
   }
 }
 
+/*      SRDELAY()
+    Pauses code execution for the period corresponding to TobiPro::_dt, the inverse of your sample rate.
+    INPUTS:   - None.
+    OUTPUTS:  - None.
+    UPDATES:  - None.
+    EFFECTS:  - Suspends code execution for 1/Fs seconds (TobiPro::_dt).
+*/
 void TobiPro::srDelay(){
   delay(TobiPro::_dt);
 }
 
-
+/*      GETSPEED()
+    Returns the speed of the specified motor in encoder_vals/sec.
+    INPUTS:   - int motor.
+    OUTPUTS:  - int motorSpeed.
+    UPDATES:  - None.
+    EFFECTS:  - None.
+*/
 int TobiPro::getSpeed(int motor){
   return TobiPro::_motorSpeed[motor];
 }
@@ -93,11 +134,27 @@ int TobiPro::calcSpeed(int motor){
     else                                      return rawSpeed;
 }
 
-void TobiPro::setSampleRate(int dtInMs){
-  TobiPro::_dt = dtInMs;
+/*      SETSAMPLERATE()
+    Sets sample rate of TobiPro object.
+    INPUTS:   - int Fs (sample rate, in Hz).
+    OUTPUTS:  - None.
+    UPDATES:  - TobiPro::_dt.
+    EFFECTS:  - None.
+*/
+
+void TobiPro::setSampleRate(int Fs){
+  TobiPro::_dt = 1000*(1/Fs);
 }
 
+/*      SETSPEED()
+    Sets speed of a motor to a certain percentaghe of its maximum speed.
+    INPUTS:   - int motor, float percent.
+    OUTPUTS:  - None.
+    UPDATES:  - TobiPro::_motorSpeed[motor].
+    EFFECTS:  - Changes servo angular velocity.
+*/
 void TobiPro::setSpeed(int motor, float percent){
   int pwm = (int)(percent * 2.55);
   TobiPro::setPwm(motor, pwm);
+  TobiPro::_motorSpeed[TobiPro::_motorIndices[motor]] = pwm;
 }
